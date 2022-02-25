3045defc7326e22c0421128da5c0fc8bcae986b5
zlatanglor <oshovskiy.d@gmail.com>
2022-02-25 16:15:13 +0200

    fixed

--- 2.Strange_calculator/scripts.js
+++ 2.Strange_calculator/scripts.js
@@ -1,11 +1,11 @@
 'use strict';
-let a = +prompt('Число "a"', '2');
-let b = +prompt('Число "b"', '3');
+let a = +prompt('Число "a"', 2);
+let b = +prompt('Число "b"', 3);
 const addition = a + b;
-const subtraction = a - b;
-const multiplication = a * b;
-const division = a / b;
 console.log('Addition:', addition);
+const subtraction = a - b;
 console.log('Subtraction:', subtraction);
+const multiplication = a * b;
 console.log('Multiplication:', multiplication);
+const division = a / b;
 console.log('Division:', division);
