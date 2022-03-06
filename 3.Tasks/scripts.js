61ade22ec204d3f2b47a2670a0b4e3fe0e20a406
zlatanglor <oshovskiy.d@gmail.com>
2022-03-06 15:57:37 +0200

    №3

--- 3.Tasks/scripts.js
+++ 3.Tasks/scripts.js
@@ -8,4 +8,4 @@ const planetEarth = 'Планета Земля';
 const userName = 'Джон';
 // Task #3
 const numberOrString = '10' + 10;
-console.log(typeof numberOrString);
+console.log(typeof numberOrString); //один из операндов является строкой — вызывается преобразование в строку.
