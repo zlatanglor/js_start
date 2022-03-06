63e3fb8471e9772992b51e742fddc63e18241164
zlatanglor <oshovskiy.d@gmail.com>
2022-03-06 14:09:59 +0200

    add Tasks

---
+++ 3.Tasks/scripts.js
@@ -0,0 +1,11 @@
+'use strict';
+// Task #1
+const name = 'Джон';
+const admin = name;
+alert(admin);
+// Task #2
+const planetEarth = 'Планета Земля';
+const userName = 'Джон';
+// Task #3
+const numberOrString = '10' + 10;
+console.log(typeof numberOrString);
