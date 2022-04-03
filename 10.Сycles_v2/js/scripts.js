645c5599d7c6c002642b571d33c3bbb2e8f8a9bd
zlatanglor <oshovskiy.d@gmail.com>
2022-04-03 10:46:14 +0300

    fix

--- 10.Сycles_v2/js/scripts.js
+++ 10.Сycles_v2/js/scripts.js
@@ -1,9 +1,9 @@
 'use strict';
 const num = +prompt('Введи число');
 let simpleNum = `${true} (простое)`;
-if (isNaN(num)) {
-    alert('Введи число!')
-}else {
+if (isNaN(num) || num <= 1) {
+    alert('Введи число больше единицы!')
+} else {
     for (let i = 2; i < num; i++) {
         if (num % i === 0) {
             simpleNum = `${false} (составное)`;
