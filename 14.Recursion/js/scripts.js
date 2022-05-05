dfb588d8969365a2232bbfb0a2deb6500d17a974
zlatanglor <oshovskiy.d@gmail.com>
2022-05-05 21:49:17 +0300

    add IIFE + arrow

--- 14.Recursion/js/scripts.js
+++ 14.Recursion/js/scripts.js
@@ -1,28 +1,39 @@
 'use strict';
-//1
-function getFactorial(n) {
-    return n !== 1 ? n * getFactorial(n - 1) : n;
-}
-
-console.log(getFactorial(4));
+(function () {
+    //1
+// function getFactorial(n) {
+//     return n !== 1 ? n * getFactorial(n - 1) : n;
+// }
+    const getFactorial = n => n !== 1 ? n * getFactorial(n - 1) : n;
+    console.log(getFactorial(4));
 
 //2
-function pow(num, degree) {
-    return degree === 1 ? num : num * pow(num, degree - 1);
-}
-
-console.log(pow(3, 3));
+// function pow(num, degree) {
+//     return degree === 1 ? num : num * pow(num, degree - 1);
+// }
+    const pow = (num, degree) => degree === 1 ? num : num * pow(num, degree - 1);
+    console.log(pow(3, 3));
 
 //3
-function sum (a, b) {
-    if (a === 0) {
-        return b;
+// function sum (a, b) {
+//     if (a === 0) {
+//         return b;
+//     }
+//     if (b === 0 ) {
+//         return a;
+//     }
+//     let i = sum(a - 1, b);
+//     return ++i;
+// }
+    const sum = (a, b) => {
+        if (a === 0) {
+            return b;
+        }
+        if (b === 0 ) {
+            return a;
+        }
+        let i = sum(a - 1, b);
+        return ++i;
     }
-    if (b === 0 ) {
-        return a;
-    }
-    let i = sum(a - 1, b);
-    return ++i;
-}
-
-console.log(sum(2, 1))
+    console.log(sum(2, 1))
+})();
